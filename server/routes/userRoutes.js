3d31e2d590f2f765ad20ae846e0548dd05eb2465
Preeti Gautam <preetigautam0411@gmail.com>
2025-08-16 20:07:51 +0530

    updated backend url

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { verifyToken } from '../middlewares/verifyUser.js';
 import { updateUser, deleteUser, getBillPreferences } from '../controllers/userController.js';
 import { metricsLogger } from '../middlewares/metricsLogger.js';
-import { csrfMiddleware } from '../middlewares/csrfMiddleware.js';
+// import { csrfMiddleware } from '../middlewares/csrfMiddleware.js';
 import { updateBillPreferences } from '../controllers/userController.js';
 
 const router = express.Router();
@@ -12,8 +12,8 @@ router.use(verifyToken); // Protect all user routes
 
 
 // Protect routes that modify data with CSRF middleware
-router.patch('/update/:userId',csrfMiddleware, updateUser);
-router.delete('/delete/:userId', csrfMiddleware, deleteUser);
+router.patch('/update/:userId', updateUser);
+router.delete('/delete/:userId', deleteUser);
 router.put('/preferences/:userId', updateBillPreferences);
 
 // The GET request for fetching preferences does not need CSRF protection
